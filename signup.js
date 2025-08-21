98cef8592e0645fd69f6c3fd7fc8240eb4f7fd8f
mohammad arif ansari <arifhjp65@gmail.com>
2025-08-21 21:05:08 +0530

    Remove delay before redirecting to login page after account creation

--- signup.js
+++ signup.js
@@ -23,5 +23,5 @@ function signup(event) {
 
   document.getElementById("msg").style.color = "green";
   document.getElementById("msg").innerText = "Account created! Go to login.";
-  setTimeout(() => window.location.href = "login.html", 1500);
+   window.location.href ="login.html";
 }
